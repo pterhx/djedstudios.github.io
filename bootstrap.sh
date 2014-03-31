956cc7e2f3e5ff6f6d919d1aae6c8ca3ab3a6f05
Jeremy Derr <jcderr@djed.com>
2014-03-31 09:53:51 -0500

    refactored to use homebrew casks

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,22 @@
 
 export ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future
 
-echo "Installing Fabric..."
-sudo easy_install pip
-sudo pip install fabric
 echo "Setting up Developer Tools CLI..."
 sudo xcode-select --install
 sudo java
-echo "Beginning install of important software..."
-curl -fsSL https://raw.githubusercontent.com/DjedStudios/getting-started/master/setup.py | python
+
+echo "Installing Homebrew..."
+#curl -fsSL https://raw.githubusercontent.com/DjedStudios/getting-started/master/setup.py | python
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+echo "Installing Cask..."
+brew install phinze/cask/brew-cask
+
+echo "Installing apps..."
+brew cask install pycharm
+brew cask install hipchat
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install pgadmin3
+brew cask install github
+brew cask install sourcetree
